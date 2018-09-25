af8f028bbf9929f02fcf4d5b53d30186ded1e53c
SreejithNS <nssreejith229@gmail.com>
2018-09-25 14:37:31 +0000

    Initial commit

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 var ip = require("ip");
 var port = 8080;
 
-http.listen(port, function() {
+
+app.use('/', express.static(__dirname + '/public/www/'));
+app.listen(port,function(err) {
    console.log('http      server.js: URL http://' + ip.address() + ':'+port+'/');
+   if(err) {
+    console.log('http      server.js: Error at http://' + ip.address() + ':'+port+'/');
+    console.log('http      server.js: Error : '+err);
+ }
 });
+
